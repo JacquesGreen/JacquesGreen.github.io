4c354e7d89f952c46d17940ad1220023136e123a
JacquesGreen <69080798+JacquesGreen@users.noreply.github.com>
2024-09-18 15:17:15 +0100

    Create layout.php

---
+++ layout.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="styles.css"/>
+		<meta charset="utf-8" />
+		<title><?php echo $title; ?> </title> <!-- variable for title -->
+	</head>
+	<body>
+	<header>
+		<section>
+			<h1>Kick⚽ff</h1>
+
+		</section>
+	</header>
+
+<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="Teams.php">Teams</a></li>
+			<li><a href="Matches.php">Matches</a></li>
+			<li><a href="Login.php">Login or register</a></li>
+            <li><a href="admin.php">admin</a></li>
+
+		</ul>
+
+	</nav>
